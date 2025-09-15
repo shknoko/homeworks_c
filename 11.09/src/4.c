4953bb989e4f7ded069754ae65491183dcb69ad7
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-09-15 17:56:45 +0300

    Lucky tickets count

--- 11.09/src/4.c
+++ 11.09/src/4.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
+    int* sums_count = malloc(28 * sizeof(int));
+
+    for (int i = 0; i < 10; i++) {
+        for (int o = 0; o < 10; o++) {
+            for (int k = 0; k < 10; k++) {
+                sums_count[i + o + k]++;
+            }
+        }
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < 28; i++) {
+        cnt += sums_count[i] * sums_count[i];
+    }
+
+    printf("%d\n", cnt);
+
     return 0;
 }
