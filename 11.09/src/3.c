5f497b4a93e9d0a748ae6a6087a1b2e8a1604f47
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-10-27 19:01:13 +0300

    Rewrited reverse_array function, fixed codestyle

--- 11.09/src/3.c
+++ 11.09/src/3.c
@@ -1,51 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *reverse_array(int a, int b, int *array);
+int reverseArray(unsigned a, unsigned b, int *array);
 
-int *reverse_array(int a, int b, int *array)
+int reverseArray(unsigned a, unsigned b, int *array)
 {
-    // если a = 0, то считается, что переворачиваем весь массив
-    // если b = 0, то считается, что переворачиваем первую часть
-    // если ни a, ни b не равны 0, то считается, что переворачиваем вторую часть длиной b
-
-    int shift = 0;
-
-    if (b == 0) {
-        b = a;
-        a = 0;
-    }
-    else if (a != 0 && b != 0) {
-        shift = a;
-        a = 0;
+    if (b < a) {
+        return 1;
     }
 
-    for (int i = a; i < b / 2; i++) {
-        int temp = array[shift + i];
-        array[shift + i] = array[shift + a + b - i - 1];
-        array[shift + a + b - i - 1] = temp;
+    for (unsigned i = 0; i < (b - a + 1) / 2; i++) {
+        int temp = array[a + i];
+        array[a + i] = array[b - i];
+        array[b - i] = temp;
     }
 
-    return array;
+    return 0;
 }
 
 int main(void)
 {
-    unsigned int m = 0;
-    unsigned int n = 0;
+    unsigned m = 0;
+    unsigned n = 0;
     scanf("%u%u", &m, &n);
 
-    int *array = malloc((n + m) * sizeof(int));
+    int* array = calloc(n + m, sizeof(int));
 
-    for (int i = 0; i < n + m; i++) {
+    for (unsigned i = 0; i < n + m; i++) {
         scanf("%d", &array[i]);
     }
 
-    array = reverse_array(m, 0, array);
-    array = reverse_array(m, n, array);
-    array = reverse_array(0, m + n, array);
+    if (((m != 0) && (n != 0))) {
+        if ((reverseArray(0, m - 1, array) != 0) || (reverseArray(m, m + n - 1, array) != 0) || (reverseArray(0, m + n - 1, array) != 0)) {
+            return 1;
+        }
+    }
 
-    for (int i = 0; i < n + m; i++) {
+    for (unsigned i = 0; i < n + m; i++) {
         printf("%d ", array[i]);
     }
 
