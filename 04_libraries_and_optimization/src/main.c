a4fcf374deebcb040ee3bca8e6ab6a9486e7f82b
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-11-01 00:19:31 +0300

    fixed codestyle

--- 04_libraries_and_optimization/src/main.c
+++ 04_libraries_and_optimization/src/main.c
@@ -5,13 +5,13 @@
 int main(void)
 {
     int len = 0;
-    int *tempArray = malloc(100 * sizeof(int));
+    int* tempArray = malloc(100 * sizeof(int));
 
     while (scanf("%d", &tempArray[len]) == 1) {
         len += 1;
     }
 
-    int *array = realloc(tempArray, len * sizeof(int));
+    int* array = realloc(tempArray, len * sizeof(int));
     int cnt = selectionSort(array, len);
 
     free(array);
