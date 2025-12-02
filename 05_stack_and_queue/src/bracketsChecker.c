46d6e710211e8b670ef66ea14dfabaa3c2b8ebf7
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-12-03 02:39:01 +0300

    Fixed codestyle

--- 05_stack_and_queue/src/bracketsChecker.c
+++ 05_stack_and_queue/src/bracketsChecker.c
@@ -1,8 +1,9 @@
 #include "stack.h"
-#include <stdio.h>
+
 #include <stdbool.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int bracketsChecker(char* str, bool* res)
 {
--- 05_stack_and_queue/src/sortStation.c
+++ 05_stack_and_queue/src/sortStation.c
@@ -1,8 +1,9 @@
 #include "stack.h"
-#include <stdio.h>
-#include <string.h>
+
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int getPriority(char operation)
 {
